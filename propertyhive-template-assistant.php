cf55b17582cc910cdbf9412c0f9c193da144136e
Steve Marks <info@wp-property-hive.com>
2025-06-02 06:53:28 +0100

    Allow additional fields to be added to property Marketing tab

--- propertyhive-template-assistant.php
+++ propertyhive-template-assistant.php
@@ -5589,6 +5589,8 @@ final class PH_Template_Assistant {
             $options['property_commercial_details'] = __( 'Property Commercial Details', 'propertyhive' );
         }
 
+        $options['property_marketing'] = __( 'Property Marketing', 'propertyhive' );
+
         if ( get_option('propertyhive_module_disabled_contacts', '') != 'yes' )
         {
             $options['contact_correspondence_address'] = __( 'Contact Correspondence Address', 'propertyhive' );
